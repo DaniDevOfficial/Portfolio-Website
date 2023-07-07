9ba4ddbc52732bb71b7490b829bfb74fdaeb308d
David Bischof <79514091+David21092@users.noreply.github.com>
2023-07-07 14:57:53 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Iframe from 'react-iframe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div >
+    <div style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }}>
+      <Iframe
+        url="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FjnGri2p3JIt1Shemk12h57%2FPortfolio%3Ftype%3Ddesign%26node-id%3D0%253A1%26mode%3Ddesign%26t%3DPHPIifhWg5d5UCBn-1"
+        width="800px"
+        height="450px"
+        allowFullScreen
+      />
+    </div>   
+     </div>
   );
 }
 
