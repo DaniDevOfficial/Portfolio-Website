65e45233417492c3f67cb5922915631615295bec
David <bischof.david.db@gmail.com>
2023-07-31 21:42:44 +0200

    Text writer and Profilepic

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,15 +1,51 @@
 // Pages and Comps
 import '../Css/Homepage.css'
+import ProfilePic from '../Images/TempProfielbild.png'
 // Libraries
+import Typewriter from "typewriter-effect";
 
 import React from 'react';
 
 function Homepage() {
   return (
     <div className='homewrapper'>
-
-        
+    <div className='typewriter'>
+      <span className='greeting'>Hey, my name is David and </span>
+      <Typewriter
+        options={{ loop: true }}
+        onInit={(typewriter) => {
+          typewriter
+            .typeString("I'm currently a student")
+            .pauseFor(1000)
+            .deleteAll()
+            .typeString("I enjoy building Websites")
+            .pauseFor(1000)
+            .deleteAll()
+            .typeString("I'm a passionate developer")
+            .pauseFor(1000)
+            .deleteAll()
+            .typeString("I love working with React and JavaScript")
+            .pauseFor(1000)
+            .deleteAll()
+            .typeString("I like dogs 🐶")
+            .pauseFor(1000)
+            .deleteAll()
+            .typeString("I'm always eager to learn new technologies")
+            .pauseFor(1000)
+            .deleteAll()
+            .typeString("I believe in the power of code to change the world")
+            .start();
+        }}
+      />
     </div>
+    <div className='content-container'>
+      <div className='portrait'>
+        <img src={ProfilePic} alt='Portrait' />
+      </div>
+      <div className='textbox'>
+      </div>
+    </div>
+  </div>
   );
 }
 
