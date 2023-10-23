1bf1ffbd1a60c602bf2ee8641852fbc91a9f5bc1
David <bischof.david.db@gmail.com>
2023-10-23 20:09:41 +0200

    added sum docu

--- src/Pages/Documentations/WishChatDocumentation.jsx
+++ src/Pages/Documentations/WishChatDocumentation.jsx
@@ -4,17 +4,29 @@ import "../../Css/Documentation.css"
 export function WishChatDocumentation() {
 
     return (
-                <div className="CenterContent"> {/* Added a new class for centering content */}
-                    <div className="Title">WishChat</div>
-                    <img className="LogoDocumentation" src={Wishchat} alt="wasd" />
+        <div className="CenterContent">
+            <div className="FirstDocumentContainer">
+                <div className="Title">WishChat</div>
+                <img className="LogoDocumentation" src={Wishchat} alt="wasd" />
 
 
-                    <div className="ShortDescription">
+                <div className="ShortDescription">
                     <div className="Author">
                         Created by: David Bischof
                     </div>
+                </div>
+            </div>
+            <div className="DescriptionAndWhyContainer">
+                <div className="DescriptionContainer">
+                    <div className="Subtitle">
+                        Description
+                    </div>
+                    <div className="Text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste vero saepe tempore recusandae adipisci delectus aperiam? Veniam aliquid praesentium saepe, sequi, consequuntur quidem nam nesciunt voluptatem sapiente rem blanditiis nisi!
                     </div>
                 </div>
+            </div>
+        </div>
 
     );
 }
