19bf053e9da5109ae8e58e336e4495998262535e
David21092 <bischof.david.db@gmail.com>
2023-10-24 10:13:08 +0200

    added basic layouting for Documentations

--- src/Pages/Documentations/WishChatDocumentation.jsx
+++ src/Pages/Documentations/WishChatDocumentation.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Wishchat from '../../Images/ActualImages/WishchatWithBackground.png'
+import WishChatSketch from '../../Images/ActualImages/WishChatSketch.png'
 import "../../Css/Documentation.css"
 export function WishChatDocumentation() {
 
@@ -23,6 +24,42 @@ export function WishChatDocumentation() {
                     </div>
                     <div className="Text">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste vero saepe tempore recusandae adipisci delectus aperiam? Veniam aliquid praesentium saepe, sequi, consequuntur quidem nam nesciunt voluptatem sapiente rem blanditiis nisi!
+                    </div>
+                </div>
+            </div>
+            <div className="PlaningAndIdea">
+                <div className="Idea">                <div className="Subtitle">
+                    Idea
+                </div>
+                    <div className="Text">
+                        <img src={WishChatSketch} alt="Image" className="ImageInText" />
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nam fuga placeat maiores cupiditate quis sunt mollitia soluta culpa repellat beatae cum, odio blanditiis! Maiores perspiciatis non beatae nulla cumque odio laboriosam qui saepe aperiam, sunt quibusdam delectus minus. A maiores, molestias voluptates molestiae quod, aut odio, ad voluptas obcaecati pariatur aliquam reprehenderit libero culpa enim. Placeat ratione laudantium repellendus earum accusantium illo aspernatur mollitia iure ex quia perferendis natus voluptatum, quidem quisquam error expedita illum animi, iste distinctio eius? Officia eos sunt illum quam sit porro quae incidunt possimus, error ab quis, a quasi aspernatur maxime eius facere voluptas?
+                    </div>
+                </div>
+                <div className="Planing">
+                    <div className="Subtitle">
+                        Planing and Development
+                    </div>
+                    <div className="Text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nam fuga placeat maiores cupiditate quis sunt mollitia soluta culpa repellat beatae cum, odio blanditiis! Maiores perspiciatis non beatae nulla cumque odio laboriosam qui saepe aperiam, sunt quibusdam delectus minus. A maiores, molestias voluptates molestiae quod, aut odio, ad voluptas obcaecati pariatur aliquam reprehenderit libero culpa enim. Placeat ratione laudantium repellendus earum accusantium illo aspernatur mollitia iure ex quia perferendis natus voluptatum, quidem quisquam error expedita illum animi, iste distinctio eius? Officia eos sunt illum quam sit porro quae incidunt possimus, error ab quis, a quasi aspernatur maxime eius facere voluptas?
+                    </div>
+                </div>
+            </div>
+            <div className="LearntAndTechStack">
+                <div className="Leant">
+                    <div className="Subtitle">
+                        What I learnt
+                    </div>
+                    <div className="Text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nam fuga placeat maiores cupiditate quis sunt mollitia soluta culpa repellat beatae cum, odio blanditiis! Maiores perspiciatis non beatae nulla cumque odio laboriosam qui saepe aperiam, sunt quibusdam delectus minus. A maiores, molestias voluptates molestiae quod, aut odio, ad voluptas obcaecati pariatur aliquam reprehenderit libero culpa enim. Placeat ratione laudantium repellendus earum accusantium illo aspernatur mollitia iure ex quia perferendis natus voluptatum, quidem quisquam error expedita illum animi, iste distinctio eius? Officia eos sunt illum quam sit porro quae incidunt possimus, error ab quis, a quasi aspernatur maxime eius facere voluptas?
+                    </div>
+                </div>
+                <div className="TechStack">
+                    <div className="Subtitle">
+                        TechStack
+                    </div>
+                    <div className="Text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem id ullam ex temporibus qui fuga perferendis esse, eligendi reiciendis. Ullam, ad? Similique quas nemo vel animi laudantium, non numquam vero cumque rem aliquid neque doloremque consequatur dolores eligendi? Quidem nihil aliquid, aliquam illo harum sunt incidunt voluptatem quae esse. Dolore similique, ab error autem ut a tempore itaque. Excepturi, molestias culpa? Blanditiis, error exercitationem! Vel ullam tenetur optio! Nam neque maiores nostrum veritatis asperiores. Sit doloremque ad facere similique cumque, quasi incidunt rerum tenetur, repellat, voluptatibus vel? Laboriosam ipsa voluptatem recusandae distinctio, harum quas nemo nesciunt, iste explicabo et in!
                     </div>
                 </div>
             </div>
